5c05e0e95276974664459d35d9257066d499f5f8
qlyan <benben198805@126.com>
2020-07-13 23:30:14 +0800

    remove uuid

--- src/main/java/LockerRobot/Ticket.java
+++ src/main/java/LockerRobot/Ticket.java
@@ -1,13 +1,9 @@
 package LockerRobot;
 
-import java.util.UUID;
-
 public class Ticket {
-    private final UUID uuid;
     private BoxSize size;
 
     public Ticket(BoxSize size) {
-        this.uuid = UUID.randomUUID();
         this.size = size;
     }
 
