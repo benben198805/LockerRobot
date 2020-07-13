ca512073271a9852c2389f4569f5ea59cca6eef1
qlyan <benben198805@126.com>
2020-07-13 23:27:53 +0800

    should save and take bag form manager

--- src/test/java/LockerRobot/LockerRobotManagerTest.java
+++ src/test/java/LockerRobot/LockerRobotManagerTest.java
@@ -54,4 +54,26 @@ public class LockerRobotManagerTest {
 
         Assert.assertEquals(bag, lockerRobotManager.take(ticket));
     }
+
+    @Test
+    public void should_save_m_m_l_s_bag_and_take_s_l_m_m_bag() {
+        LockerRobotManager lockerRobotManager = new LockerRobotManager();
+
+        Bag bag1 = new Bag(BoxSize.SIZE_L);
+        Ticket ticket1 = lockerRobotManager.save(bag1);
+
+        Bag bag2 = new Bag(BoxSize.SIZE_M);
+        Ticket ticket2 = lockerRobotManager.save(bag2);
+
+        Bag bag3 = new Bag(BoxSize.SIZE_M);
+        Ticket ticket3 = lockerRobotManager.save(bag3);
+
+        Bag bag4 = new Bag(BoxSize.SIZE_S);
+        Ticket ticket4 = lockerRobotManager.save(bag4);
+
+        Assert.assertEquals(bag4, lockerRobotManager.take(ticket4));
+        Assert.assertEquals(bag1, lockerRobotManager.take(ticket1));
+        Assert.assertEquals(bag2, lockerRobotManager.take(ticket2));
+        Assert.assertEquals(bag3, lockerRobotManager.take(ticket3));
+    }
 }
